48b947f33970583f78ae3a453a07bb11b7f5d7ec
JelteMX <jelte.lagendijk@mendix.com>
2017-06-21 10:20:06 +0200

    Update CF cli & plugin

--- _scripts/before_deploy.sh
+++ _scripts/before_deploy.sh
@@ -14,7 +14,8 @@ then
       cf api $CF_API
       cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
       cf -v
-      cf install-plugin 'https://github.com/contraband/autopilot/releases/download/0.0.2/autopilot-linux' -f
+      # cf install-plugin 'https://github.com/contraband/autopilot/releases/download/0.0.2/autopilot-linux' -f
+      cf install-plugin './autopilot-linux' -f
   else
       exit 0
   fi

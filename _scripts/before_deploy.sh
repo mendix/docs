3feca4d678c8d060ee88aa3b6c374a20dfd25d87
JelteMX <jelte.lagendijk@mendix.com>
2017-07-26 06:45:11 +0200

    Remove testing branch

--- _scripts/before_deploy.sh
+++ _scripts/before_deploy.sh
@@ -16,13 +16,6 @@ then
       cf -v
       # cf install-plugin 'https://github.com/contraband/autopilot/releases/download/0.0.2/autopilot-linux' -f
       cf install-plugin "$HOME/autopilot-linux" -f
-  elif [[ $TRAVIS_BRANCH == 'travis_improvements' ]]; then
-      echo 'Installing autopilot'
-      cf api $CF_API
-      cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
-      cf -v
-      # cf install-plugin 'https://github.com/contraband/autopilot/releases/download/0.0.2/autopilot-linux' -f
-      cf install-plugin "$HOME/autopilot-linux" -f
   else
       exit 0
   fi

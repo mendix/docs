ebaf77443851b7deb8284bfa339d2055b22c0a97
JelteMX <jelte.lagendijk@mendix.com>
2017-07-26 06:33:39 +0200

    Updates

--- _scripts/before_deploy.sh
+++ _scripts/before_deploy.sh
@@ -15,14 +15,14 @@ then
       cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
       cf -v
       # cf install-plugin 'https://github.com/contraband/autopilot/releases/download/0.0.2/autopilot-linux' -f
-      cf install-plugin './_scripts/autopilot-linux' -f
+      cf install-plugin "$HOME/autopilot-linux" -f
   elif [[ $TRAVIS_BRANCH == 'travis_improvements' ]]; then
       echo 'Installing autopilot'
       cf api $CF_API
       cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
       cf -v
       # cf install-plugin 'https://github.com/contraband/autopilot/releases/download/0.0.2/autopilot-linux' -f
-      cf install-plugin './_scripts/autopilot-linux' -f
+      cf install-plugin "$HOME/autopilot-linux" -f
   else
       exit 0
   fi

66f0c97fef62d6a1f8269c309e36e797a66907ec
Maria Shaposhnikova <maria.shaposhnikova@mendix.com>
2018-06-21 09:30:55 +0200

    redirect update

--- _assets/mappings/redirect.js
+++ _assets/mappings/redirect.js
@@ -7521,7 +7521,7 @@ module.exports = {
     },
     {
       "from": "/refguide7/Modeler",
-      "to": "/refguide/modeler"
+      "to": "/refguide/desktop-modeler"
     },
     {
       "from": "/refguide7/Module+Role",

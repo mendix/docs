defe1e30d5f5ef4f4b723b850fbec170ddcc980d
Adam Dupaski <adam.dupaski@mendix.com>
2018-11-13 15:21:47 +0100

    Remove IoT app tutorial

--- _assets/mappings/redirect.js
+++ _assets/mappings/redirect.js
@@ -2072,10 +2072,6 @@ module.exports = {
      * HOWTO MENDIX 7
      ****************************************************/
     {
-      from: "/howto/getting-started/build-an-iot-app",
-      to: "/howto/tutorials/build-an-iot-app"
-    },
-    {
       from: "/howto/getting-started/build-a-simple-hrm-app",
       to: "/howto/tutorials/build-a-simple-hrm-app"
     },

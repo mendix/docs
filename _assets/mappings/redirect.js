756f49dfb2a753d4db917d061d53c40099e914d9
Adam Dupaski <adam.dupaski@mendix.com>
2018-12-20 11:54:02 +0100

    no message

--- _assets/mappings/redirect.js
+++ _assets/mappings/redirect.js
@@ -506,6 +506,10 @@ module.exports = {
       from: "/mendixcloud/java-in-the-cloud",
       to: "/developerportal/deploy/java-in-the-cloud"
     },
+	{
+      from: "/howto50/Contributing+to+a+GitHub+repository",
+      to: "/howto/collaboration-requirements-management/contribute-to-a-github-repository"
+    },
 	/****************************************************
      * For Support - 12.19.18, Verify if Still Necessary
      ****************************************************/

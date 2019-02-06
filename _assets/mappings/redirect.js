4b9aaacc5f0f7161377821d3e6559b9515f2f0ed
Adam Dupaski <adam.dupaski@mendix.com>
2019-02-06 13:38:59 +0100

    Update redirects

--- _assets/mappings/redirect.js
+++ _assets/mappings/redirect.js
@@ -93,10 +93,18 @@ module.exports = {
 	{
       from: "/refguide/certificates",
       to: "/developerportal/deploy/certificates"
-    },	
+    },
+	{
+      from: "/howto/deploying-a-mendix-app-to-cloud-foundry",
+      to: "/developerportal/deploy/cloud-foundry-deploy"
+    },
 	/****************************************************
-     * App Store (mapped, unversioned, temporary until fixed in product)
+     * From the App Store (mapped, unversioned, temporary until fixed in product)
      ****************************************************/
+	{
+	  from: "/community/app-store/use-app-store-content-in-the-modeler",
+	  to: "/developerportal/app-store/use-app-store-content-in-the-modeler"
+    },
 	{
       from: "/howto/collaboration-project-management/contribute-to-a-github-repository",
       to: "/howto/collaboration-requirements-management/contribute-to-a-github-repository"
@@ -144,11 +152,42 @@ module.exports = {
       from: "/refguide/web-modeler/app-settings-wm",
       to: "/web-modeler/app-settings-wm"
     },
+	{
+      from: "/howto/start-with-a-blank-app",
+      to: "/web-modeler/general-wm"
+    },
+    {
+      from: "/howto/tutorials/create-a-to-do-app",
+      to: "/web-modeler/general-wm"
+    },
+    {
+      from: "/howto/tutorials/build-a-simple-hrm-app",
+      to: "/web-modeler/general-wm"
+    },
+    {
+      from: "/howto/tutorials/build-an-iot-app",
+      to: "/web-modeler/general-wm"
+    },
+	/****************************************************
+     * How-to's version 7 (unmapped, temporary)
+     ****************************************************/
+    {
+      from: "/howtogeneral/bestpractices/ux-best-practices",
+      to: "/howto/ux/ux-best-practices"
+    },
+	{
+      from: "/howtogeneral/bestpractices/best-practices-security-and-improvements-for-mendix-applications",
+      to: "/howto/security/best-practices-security"
+    },
 	/****************************************************
      * From Desktop Modeler version 7 (mapped, permanent)
      ****************************************************/
 	{
       from: "/refguide/modeler",
+      to: "/refguide/desktop-modeler-overview"
+    },
+	{
+      from: "/refguide/Modeler",
       to: "/refguide/desktop-modeler-overview"
     },
 	{
@@ -184,39 +223,16 @@ module.exports = {
       to: "/howto/monitoring-troubleshooting/solving-load-and-import-errors"
     },
 	/****************************************************
-     * Former Tutorials (unmapped but permanent until verified as temporary)
-     ****************************************************/
-	{
-      from: "/howto/start-with-a-blank-app",
-      to: "/web-modeler/general-wm"
-    },
-    {
-      from: "/howto/tutorials/create-a-to-do-app",
-      to: "/web-modeler/general-wm"
-    },
-    {
-      from: "/howto/tutorials/build-a-simple-hrm-app",
-      to: "/web-modeler/general-wm"
-    },
-    {
-      from: "/howto/tutorials/build-an-iot-app",
-      to: "/web-modeler/general-wm"
-    },
-	/****************************************************
-     * How-to's (unmapped, unversioned, temporary)
+     * Reference Guide version 6
      ****************************************************/
     {
-      from: "/howtogeneral/bestpractices/ux-best-practices",
-      to: "/howto/ux/ux-best-practices"
-    },
+      from: "/refguide6/Reference+Guide+6",
+      to: "/refguide6/"
+	},
 	{
-      from: "/howtogeneral/bestpractices/best-practices-security-and-improvements-for-mendix-applications",
-      to: "/howto/security/best-practices-security"
-    },
-    {
-      from: "/howto/deploying-a-mendix-app-to-cloud-foundry",
-      to: "/developerportal/deploy/cloud-foundry-deploy"
-    },
+      from: "/refguide6/Modeler",
+      to: "/refguide6/modeler"
+    },	
 	/****************************************************
      * APM (unmapped, permanent)
      ****************************************************/

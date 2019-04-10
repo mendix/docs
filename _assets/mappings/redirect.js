46fa8fee8cdc778d5fd16234daadc1bd07f181da
Adam Dupaski <adam.dupaski@mendix.com>
2019-04-10 13:35:53 +0200

    Revert "Try 7 in redirects"
    This reverts commit 369c027a9b4245cd5e9dfd4fdce667cf611450c3.

--- _assets/mappings/redirect.js
+++ _assets/mappings/redirect.js
@@ -228,7 +228,7 @@ module.exports = {
      ****************************************************/
 	{
       from: "/refguide7/modeler",
-      to: "/refguide7/desktop-modeler-overview"
+      to: "/refguide/desktop-modeler-overview"
     },
 	{
       from: "/refguide7/Modeler",

e0e9b1ae7bb9246e3278223e1390d0e71adbd8b7
mark.van.ments@mendix.com <mark.van.ments@mendix.com>
2020-11-18 09:31:10 +0100

    Test Adding Travis commands for AWS - #1

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,21 +2,28 @@
 
 set -ev
 
-if ([ "${TRAVIS_PULL_REQUEST}" == "true" ])
+if ([ "${TRAVIS_PULL_REQUEST}" == "true" -a "${TRAVIS_BRANCH}" != "MvMAWSTESTDONOTMERGE" ])
 then
   echo 'Pull request, not deploying'
   exit 0
 fi
 
+if ([ "${TRAVIS_PULL_REQUEST}" == "true" -a "${TRAVIS_BRANCH}" == "MvMAWSTESTDONOTMERGE" ])
+then
+  echo 'Testing AWS deployment'
+  pwd
+  exit 0
+fi
+
 if ([ "${TRAVIS_BRANCH}" == "development" ])
 then
-  cf push -f ./manifest_accp.yml
+  echo 'cf push -f ./manifest_accp.yml'
   exit 0
 fi
 
 if ([ "${TRAVIS_BRANCH}" == "master" ])
 then
-  cf zero-downtime-push $CF_APP -f ./manifest_prod.yml
+  echo 'cf zero-downtime-push $CF_APP -f ./manifest_prod.yml'
   exit 0
 fi
 

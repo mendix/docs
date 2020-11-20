fb7ebdb989bd2b56c2de1aa0667f45a47132c087
mark.van.ments@mendix.com <mark.van.ments@mendix.com>
2020-11-20 11:36:46 +0100

    AWS - merge latest development and set shell scripts to run in context to pick up bucket name dynamically

--- _scripts/test_deploy.sh
+++ _scripts/test_deploy.sh
@@ -20,9 +20,9 @@ then
   start=$SECONDS
   echo "Starting setup of redirection"
   chmod +x $TRAVIS_BUILD_DIR/_scripts/redirectaws.sh
-  # $TRAVIS_BUILD_DIR/_scripts/redirectaws.sh
+  # source $TRAVIS_BUILD_DIR/_scripts/redirectaws.sh # source ensures that script can read TARGETAWSBUCKET
   chmod +x $TRAVIS_BUILD_DIR/_scripts/mark_test.sh
-  $TRAVIS_BUILD_DIR/_scripts/mark_test.sh
+  source $TRAVIS_BUILD_DIR/_scripts/mark_test.sh # source ensures that script can read TARGETAWSBUCKET
   echo "Setting up redirects took $((SECONDS - start)) seconds"  
   # HUGO creates new files with a newer timestamp so this will always push all the html - this catches all single character changes at the expense of time. Rely on size only for images as these are unlikely to be the same size.
   start=$SECONDS
@@ -30,6 +30,7 @@ then
   aws s3 sync . s3://mendixtestdocumentation --delete --only-show-errors --exclude "*.[abcdefghijklmnnopqrstuvwxyz]*" --content-type text/html # Sync only html files (without file type) and set content type for html
   echo "Upload of HTML took $((SECONDS - start)) seconds"
   start=$SECONDS
+  echo "Starting sync of non-HTML files"  
   aws s3 sync . s3://mendixtestdocumentation --delete --size-only --exclude "*" --include "*.[abcdefghijklmnnopqrstuvwxyz]*" # Sync all other files and ensure that content type is not overwritten Just rely on size for all changes to these files.
   echo "Upload of non-html took $((SECONDS - start)) seconds"  
   exit 0
